a8263a6ede5528edf7e34eabc605a666a9e649ef
Gtmanic <themanicmidget@gmail.com>
2016-05-17 20:53:06 +0100

    Initial commit

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-var main = function() {
-
-    $('form').submit(function(event) {
-        var $input = $(event.target).find('input');
-        var comment = $input.val();
-
-        if (comment != "") {
-            var html = $('<li>').text(comment);
-            html.prependTo('#comments');
-            $input.val("");
-        }
-
-        return false;
-    });
-
-}
-
-$(document).ready(main);
+script.js
